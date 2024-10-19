64a470e5a1e3d73bef32ec9b3b2d59ef9d8ba4fa
Kelvison Marcos <kelvison.marcos10@gmail.com>
2024-10-19 20:59:23 -0300

    Primeiros códigos JS

--- Full Stack Pro/Javascrip/script.js
+++ Full Stack Pro/Javascrip/script.js
@@ -1,8 +1,4 @@
 
 var nome = prompt("qual é o seu nome?");
 
-var sobrenome = promt("qual é seu sobrenome?");
-
-console.log("olá" + nome + " " + sobrenome);
-
-document.write("<h1>bem-vindo" + nome + ao projeto "</h1>")
+console.log("olá" + nome);s
