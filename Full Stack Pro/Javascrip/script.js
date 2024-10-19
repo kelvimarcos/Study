868743dd0439cda4aa29e95b04db7732e7b13819
Kelvison Marcos <kelvison.marcos10@gmail.com>
2024-10-19 18:05:36 -0300

    Estudo de Lógica de programação
    Estudo de Lógica de programação

--- Full Stack Pro/Javascrip/script.js
+++ Full Stack Pro/Javascrip/script.js
@@ -1,2 +1,8 @@
 
-alert("teste")
+var nome = prompt("qual é o seu nome?");
+
+var sobrenome = promt("qual é seu sobrenome?");
+
+console.log("olá" + nome + " " + sobrenome);
+
+document.write("<h1>bem-vindo" + nome + ao projeto "</h1>")
