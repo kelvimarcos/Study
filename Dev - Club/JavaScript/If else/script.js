acc73c850cdfac824518100aae3967224afa387d
Kelvison Marcos <kelvison.marcos10@gmail.com>
2025-07-06 22:29:32 -0300

    atualização

--- Dev - Club/JavaScript/If else/script.js
+++ Dev - Club/JavaScript/If else/script.js
@@ -12,12 +12,9 @@
 
 const notadoaluno = 9
 
-
-
 if (notadoaluno === 10) { 
     console.log("Aluno nota 10")
 } 
-
 
 if (notadoaluno < 10){ 
     console.log (`Você ficou com ${notadoaluno} pontos na prova. Cara, tu é muito ruim, estuda mais`)
