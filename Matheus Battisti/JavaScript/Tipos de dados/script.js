9ef19e73438e3b0b37d8577c4f274d0c2d430827
Kelvison Marcos <kelvison.marcos10@gmail.com>
2025-07-22 05:52:45 -0300

    Curso de JavaScritp - Matehus Basttini

---
+++ Matheus Battisti/JavaScript/Tipos de dados/script.js
@@ -0,0 +1,10 @@
+
+/* Tipo de dados
+
+No JS temos diversos tipos de dados, os mais comuns são: 
+Number, string, bolean, null, undefined e Object.apply
+podemos verificar  tipo de dado com o operador. */
+
+
+/* Number - é o tipo que é dado para números;
+possui três valores simbólicos: +Infinity, -infinity e NaN */
