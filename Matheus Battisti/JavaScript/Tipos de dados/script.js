612da4fed071c75b1f04d5ad9dbd80fd5f605682
Kelvison Marcos <kelvison.marcos10@gmail.com>
2025-07-22 06:08:46 -0300

    Boleans - JS

--- Matheus Battisti/JavaScript/Tipos de dados/script.js
+++ Matheus Battisti/JavaScript/Tipos de dados/script.js
@@ -8,3 +8,23 @@ podemos verificar  tipo de dado com o operador. */
 
 /* Number - é o tipo que é dado para números;
 possui três valores simbólicos: +Infinity, -infinity e NaN */
+
+
+/* String - Tipo de textos */
+/* 
+let nome = 'kelvison';
+let sobrenome = 'Marcos';
+let idade = 27
+
+console.log(typeof nome);
+console.log(typeof idade);
+
+let mensagem = 'Olá, tudo?'
+
+
+console.log(typeof mensagem)
+ */
+
+/* Boolean - Representa um valor verdadeirou ou falso
+comparações e resultados em booleans
+ */
