525cf909654c9775b3debac4ae1dbf96f68b0ec9
Kelvison Marcos <kelvison.marcos10@gmail.com>
2025-07-27 00:07:53 -0300

    Update script.js

--- Matheus Battisti/JavaScript/Escopo/script.js
+++ Matheus Battisti/JavaScript/Escopo/script.js
@@ -1,5 +1,5 @@
 /* - No JavaScript podemos ter vários escopos;
-- O globalThis, que é iniciado em toda a aplicação;
+- O global, que é iniciado em toda a aplicação;
 - E os locais, que podem existir em várias instruções como as funções;
  */
 
